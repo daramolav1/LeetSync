1bc28c4454fb28bfdc6227615f5b89379c8f4eb2
Victor Daramola <49133983+daramolav1@users.noreply.github.com>
2023-12-28 14:18:08 -0500

    Time: 55 ms (80.74%) | Memory: 42.6 MB (48.25%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let prevMap = {}
+
+    for(let i=0;i<nums.length;i++){
+        let diff = target - nums[i]
+
+        if(diff in prevMap){
+            return [prevMap[diff], i]
+        }
+
+        prevMap[nums[i]] = i
+    }
+};
