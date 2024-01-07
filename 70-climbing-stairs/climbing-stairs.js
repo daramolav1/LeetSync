98377a75b7d5e70f4d619c8f0c11981dae36043c
Victor Daramola <49133983+daramolav1@users.noreply.github.com>
2024-01-07 18:07:34 -0500

    Time: 47 ms (79.89%) | Memory: 41.7 MB (48.80%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    let x = 1
+    let y = 1
+
+    for (let i = 0; i < n - 1; i++) {
+        let temp = x
+        x = x + y
+        y = temp
+    }
+
+    return x
+};
