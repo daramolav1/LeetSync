66a208649b84c0e4a32a16e26d566aab5e529296
Victor Daramola <49133983+daramolav1@users.noreply.github.com>
2024-01-06 22:33:21 -0500

    Time: 52 ms (78.46%) | Memory: 42.5 MB (35.31%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+    let result = 0
+
+    while (n) {
+        n &= (n - 1)
+        result += 1
+    }
+    return result
+};
